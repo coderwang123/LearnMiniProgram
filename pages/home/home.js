5c2c2b3d02adee2e09b6d27ac5f6ff0ab91e8ede
coderwang123 <277970003@qq.com>
2020-03-05 17:56:14 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,24 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:"coderwang",
+    age:18,
+    students:[
+      {id:1,name:"佐助",age:18},
+      { id: 2, name: "鸣人", age: 18 },
+      { id: 3, name: "卡卡西", age: 20 },
+      { id: 4, name: "带土", age: 30 }
+    ],
+    counter: 0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  jiabtnclick() {
+    this.setData({
+      counter:this.data.counter +1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  jianbtnclick() {
+    this.setData({
+      counter: this.data.counter -1
+    })
   }
 })
